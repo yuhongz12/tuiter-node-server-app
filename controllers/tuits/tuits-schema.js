9d74c284352a82e0b88a96a962ff9115fed50365
yuhongz12 <yuhong.zhao12@gmail.com>
2022-11-30 23:06:09 -0500

    update tuits schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,8 +1,17 @@
 import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
+  username: String,
+  time: String,
+  image: String,
+  title: String,
   tuit: String,
   likes: Number,
   liked: Boolean,
+  disliked: Boolean,
+  dislikes: Number,
+  replies: Number,
+  retuits: Number
 }, {collection: 'tuits'});
 export default schema;
+
